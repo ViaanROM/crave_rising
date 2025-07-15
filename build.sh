131e0fcef981ee11c6f271b62fc1b07fb85014a9
ViaanROM <viaan1207@gmail.com>
2025-07-15 20:39:33 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ rm -rf {device,vendor,kernel,hardware}/oneplus && \
 rm -rf prebuilts/clang/host/linux-x86 && \
 repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs && \
 /opt/crave/resync.sh && \
-git clone https://github.com/ViaanROM/android_device_oneplus_larry -b Infinity15 device/oneplus/larry && \
+git clone https://github.com/ViaanROM/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry && \
 git clone https://github.com/ViaanROM/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common && \
 git clone https://github.com/ViaanROM/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry && \
 git clone https://github.com/ViaanROM/proprietary_vendor_oneplus_sm6375-common -b lineage-22.2 vendor/oneplus/sm6375-common && \
