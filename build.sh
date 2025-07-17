4d2500aff9f530762e10c9a37a805a83e0d04f89
ViaanROM <viaan1207@gmail.com>
2025-07-17 21:14:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,31 +1,28 @@
 # Rising OS Revived for Nord CE 3 Lite 5G
+
 export USE_CCACHE=1
-
 ccache -M 20G
-rm -rf .repo/local_manifests; \
 
+# Clean up previous sources
+rm -rf .repo/local_manifests && \
 rm -rf {device,vendor,kernel,hardware}/oneplus && \
-
 rm -rf prebuilts/clang/host/linux-x86 && \
 
+# Initialize repo (only once)
 repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs && \
 
+# Sync sources
 /opt/crave/resync.sh && \
 
+# Clone device-specific trees
 git clone https://github.com/ViaanROM/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry && \
-
 git clone https://github.com/ViaanROM/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common && \
-
 git clone https://github.com/ViaanROM/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry && \
-
 git clone https://github.com/ViaanROM/proprietary_vendor_oneplus_sm6375-common -b lineage-22.2 vendor/oneplus/sm6375-common && \
-
 git clone https://github.com/ViaanROM/android_kernel_oneplus_sm6375 -b lineage-22.2 kernel/oneplus/sm6375 && \
-
 git clone https://github.com/ViaanROM/android_hardware_oplus -b lineage-22.2 hardware/oplus && \
 
-source .build/envsetup.sh && \ 
-
+# Setup environment and start build
+source build/envsetup.sh && \
 riseup larry user && \
-
-rise b
+rise b -j$(nproc)
