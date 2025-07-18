56688b5cf73f816ae4b85d0a6bd76fa7b8f0d882
ViaanROM <viaan1207@gmail.com>
2025-07-18 21:16:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,15 +17,8 @@ repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs
 
 # Sync all sources
 /opt/crave/resync.sh
-
-# Clone device-specific sources
-git clone https://github.com/ViaanROM/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry
-git clone https://github.com/ViaanROM/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common
-git clone https://github.com/ViaanROM/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry
-git clone https://github.com/ViaanROM/proprietary_vendor_oneplus_sm6375-common -b lineage-22.2 vendor/oneplus/sm6375-common
-git clone https://github.com/ViaanROM/android_kernel_oneplus_sm6375 -b lineage-22.2 kernel/oneplus/sm6375
-git clone https://github.com/ViaanROM/android_hardware_oplus -b lineage-22.2 hardware/oplus
-
+# Clone Local Manifest
+git clone https://github.com/ViaanROM/local_manifests --depth 1 -b main .repo/local_manifests
 # Setup environment and build
 source build/envsetup.sh
 riseup larry user
