9335a9825868f3e59f7d62b01e51295576e3d857
ViaanROM <viaan1207@gmail.com>
2025-07-19 10:48:08 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,5 +21,5 @@ repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs
 git clone https://github.com/ViaanROM/local_manifests --depth 1 -b main .repo/local_manifests
 # Setup environment and build
 source build/envsetup.sh
-breakfast lineage_larry user
+lunch lineage_larry user
 mka bacon
