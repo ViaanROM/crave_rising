d27427295bff15aaf1c54d40ac6b8632abc3da80
ViaanROM <viaan1207@gmail.com>
2025-07-19 11:58:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,26 +1,26 @@
 # RisingOS Revived for Nord CE 3 Lite 5G (larry)
 # Install CCACHE
-sudo apt update && sudo apt install ccache \
+sudo apt update && sudo apt install ccache ; \
 # Enable ccache for faster builds
-export USE_CCACHE=1 \
-ccache -M 20G \
+export USE_CCACHE=1 ; \
+ccache -M 20G ; \
 
 # Clean up previous sources
-rm -rf .repo/local_manifests \
-rm -rf device/oneplus \
-rm -rf vendor/oneplus \
-rm -rf kernel/oneplus \
-rm -rf hardware/oplus \
-rm -rf prebuilts/clang/host/linux-x86 \
+rm -rf .repo/local_manifests ; \
+rm -rf device/oneplus ; \
+rm -rf vendor/oneplus ; \
+rm -rf kernel/oneplus ; \
+rm -rf hardware/oplus ; \
+rm -rf prebuilts/clang/host/linux-x86 ; \
 
-# Initialize RisingOS repo (run only once)
-repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs \
+# Initialize RisingOS repo (run only once
+repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs ; \
 
 # Sync all sources
-/opt/crave/resync.sh \
+/opt/crave/resync.sh ; \
 # Clone Local Manifest
-git clone https://github.com/ViaanROM/local_manifests --depth 1 -b main .repo/local_manifests \
+git clone https://github.com/ViaanROM/local_manifests --depth 1 -b main .repo/local_manifests ; \
 # Setup environment and build
-source build/envsetup.sh \
-lunch lineage_larry-user \
-mka bacon \
+source build/envsetup.sh ; \
+lunch lineage_larry-user ; \
+mka b
