2feb5b302cec87593556fd73e83b9c061f0267b4
ViaanROM <viaan1207@gmail.com>
2025-07-23 20:20:59 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,6 +16,9 @@ rm -rf prebuilts/clang/host/linux-x86 ; \
 # Sync all sources
 repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --depth=1 --git-lfs ; \
 
+# Sync the Source Code
+repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+
 # Copy Trees, Blobs
 git clone https://github.com/ViaanROM/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry ; \
 git clone https://github.com/ViaanROM/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common ; \
