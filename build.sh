6e2e852b20e9939deadef4b7c885de9da3a12bd8
ViaanROM <viaan1207@gmail.com>
2025-07-23 18:55:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,11 +13,9 @@ rm -rf kernel/oneplus ; \
 rm -rf hardware/oplus ; \
 rm -rf prebuilts/clang/host/linux-x86 ; \
 
-# Initialize RisingOS repo (run only once
-repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs ; \
-
 # Sync all sources
 repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --depth=1 --git-lfs ; \
+
 # Copy Trees, Blobs
 git clone https://github.com/ViaanROM/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry ; \
 git clone https://github.com/ViaanROM/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common ; \
@@ -25,6 +23,7 @@ git clone https://github.com/ViaanROM/proprietary_vendor_oneplus_larry -b lineag
 git clone https://github.com/ViaanROM/proprietary_vendor_oneplus_sm6375-common -b lineage-22.2 vendor/oneplus/sm6375-common ; \
 git clone https://github.com/ViaanROM/android_kernel_oneplus_sm6375 -b lineage-22.2 kernel/oneplus/sm6375 ; \
 git clone https://github.com/ViaanROM/android_hardware_oplus -b lineage-22.2 hardware/oplus ; \
+
 # Setup environment and build
 source build/envsetup.sh ; \
 breakfast lineage_larry-user ; \
