df76e35aca9cfd98d315dda30e311bdfd338b8b9
ViaanROM <viaan1207@gmail.com>
2025-07-23 18:54:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 sudo apt update && sudo apt install ccache ; \
 # Enable ccache for faster builds
 export USE_CCACHE=1 ; \
-ccache -M 20G ; \
+ccache -M 25G ; \
 
 # Clean up previous sources
 rm -rf .repo/local_manifests ; \
@@ -17,10 +17,15 @@ rm -rf prebuilts/clang/host/linux-x86 ; \
 repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs ; \
 
 # Sync all sources
-/opt/crave/resync.sh ; \
-# Clone Local Manifest
-git clone https://github.com/ViaanROM/local_manifests --depth 1 -b main .repo/local_manifests ; \
+repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --depth=1 --git-lfs ; \
+# Copy Trees, Blobs
+git clone https://github.com/ViaanROM/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry ; \
+git clone https://github.com/ViaanROM/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common ; \
+git clone https://github.com/ViaanROM/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry ; \
+git clone https://github.com/ViaanROM/proprietary_vendor_oneplus_sm6375-common -b lineage-22.2 vendor/oneplus/sm6375-common ; \
+git clone https://github.com/ViaanROM/android_kernel_oneplus_sm6375 -b lineage-22.2 kernel/oneplus/sm6375 ; \
+git clone https://github.com/ViaanROM/android_hardware_oplus -b lineage-22.2 hardware/oplus ; \
 # Setup environment and build
 source build/envsetup.sh ; \
-lunch lineage_larry-user ; \
+breakfast lineage_larry-user ; \
 mka bacon
