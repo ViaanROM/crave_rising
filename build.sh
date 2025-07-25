6cb2554006038b5ba411f95e42b36c473356c197
ViaanROM <viaan1207@gmail.com>
2025-07-25 16:50:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,28 +1,52 @@
-# RisingOS Revived for Nord CE 3 Lite 5G (larry)
-# Clean up previous sources
-rm -rf .repo/local_manifests ; \
-rm -rf device/oneplus ; \
-rm -rf vendor/oneplus ; \
-rm -rf kernel/oneplus ; \
-rm -rf hardware/oplus ; \
-rm -rf prebuilts/clang/host/linux-x86 ; \
+# === RisingOS Revived Build Script for Nord CE 3 Lite (larry) ===
 
-# Sync all sources
-repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --depth=1 --git-lfs ; \
+# Clean previous local sources
+rm -rf .repo/local_manifests \
+       device/oneplus \
+       vendor/oneplus \
+       kernel/oneplus \
+       hardware/oplus \
+       prebuilts/clang/host/linux-x86
 
-# Sync the Source Code
+# Repo Init & Sync
+repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --depth=1 --git-lfs
 /opt/crave/resync.sh
 
-# Copy Trees, Blobs
-git clone https://github.com/ViaanROM/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry ; \
-git clone https://github.com/ViaanROM/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common ; \
-git clone https://github.com/ViaanROM/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry ; \
-git clone https://github.com/ViaanROM/proprietary_vendor_oneplus_sm6375-common -b lineage-22.2 vendor/oneplus/sm6375-common ; \
-git clone https://github.com/ViaanROM/android_kernel_oneplus_sm6375 -b lineage-22.2 kernel/oneplus/sm6375 ; \
-git clone https://github.com/ViaanROM/android_hardware_oplus -b lineage-22.2 hardware/oplus ; \
+# Clone Trees and Blobs
+git clone https://github.com/ViaanROM/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry
+git clone https://github.com/ViaanROM/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common
+git clone https://github.com/ViaanROM/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry
+git clone https://github.com/ViaanROM/proprietary_vendor_oneplus_sm6375-common -b lineage-22.2 vendor/oneplus/sm6375-common
+git clone https://github.com/ViaanROM/android_kernel_oneplus_sm6375 -b lineage-22.2 kernel/oneplus/sm6375
+git clone https://github.com/ViaanROM/android_hardware_oplus -b lineage-22.2 hardware/oplus
 
-# Setup environment and build
-source build/envsetup.sh ; \
+# ============================
+#       VANILLA BUILD
+# ============================
+
+source build/envsetup.sh
 make installclean
-croot
 brunch larry-user
+
+# Save Vanilla Build Output
+rm -rf out/target/product/vanilla
+mv out/target/product/larry out/target/product/vanilla
+
+# ============================
+#       GAPPS BUILD
+# ============================
+
+# Swap makefile
+cd device/oneplus/larry
+rm -f larry.mk
+mv gapps.txt larry.mk
+cd ../../..
+
+# Re-source and build
+source build/envsetup.sh
+make installclean
+brunch larry-user
+
+# Save GApps Build Output
+rm -rf out/target/product/gapps
+mv out/target/product/larry out/target/product/gapps
