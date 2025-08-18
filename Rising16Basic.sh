b409bc5651b2558e8dd51192f29bc7d48c07859a
ViaanROM <viaan1207@gmail.com>
2025-08-18 20:29:50 +0530

    Create Rising16Basic.sh

---
+++ Rising16Basic.sh
@@ -0,0 +1,10 @@
+repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
+/opt/crave/resync.sh
+git clone https://github.com/anshedu/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry
+git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common
+git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry
+git clone https://github.com/anshedu/proprietary_vendor_oneplus_sm6375-common -b lineage-22.2 vendor/oneplus/sm6375-common
+git clone https://github.com/anshedu/android_kernel_oneplus_sm6375 -b lineage-22.2 kernel/oneplus/sm6375
+git clone https://github.com/anshedu/android_hardware_oplus -b lineage-22.2 hardware/oplus
+riseup larry user
+rise b
