1cdb172d9a98858374d8e63c8f4273212c6b82b6
ViaanROM <viaan1207@gmail.com>
2025-08-18 20:26:22 +0530

    Update DerpFestAOSP.sh

--- DerpFestAOSP.sh
+++ DerpFestAOSP.sh
@@ -1,5 +1,5 @@
 repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16 --git-lfs
-repo sync
+/opt/crave/resync.sh
 git clone https://github.com/anshedu/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry
 git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common
 git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry
