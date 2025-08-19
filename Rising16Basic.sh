8df52fc0ba77f072ad154d9420068cefb98c19ee
ViaanROM <viaan1207@gmail.com>
2025-08-19 06:56:15 +0530

    Update Rising16Basic.sh

--- Rising16Basic.sh
+++ Rising16Basic.sh
@@ -6,5 +6,5 @@ git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage
 git clone https://github.com/anshedu/proprietary_vendor_oneplus_sm6375-common -b lineage-22.2 vendor/oneplus/sm6375-common
 git clone https://github.com/anshedu/android_kernel_oneplus_sm6375 -b lineage-22.2 kernel/oneplus/sm6375
 git clone https://github.com/anshedu/android_hardware_oplus -b lineage-22.2 hardware/oplus
-riseup larry user
-rise b
+lunch lineage_larry-user || lunch lineage_larry-bp2a-user
+m bacon
