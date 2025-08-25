0e2d5b0aeb5fea72cc9a59163260d5a09bd71b7f
ViaanROM <viaan1207@gmail.com>
2025-08-25 18:29:30 +0530

    Update DerpFestAOSP.sh

--- DerpFestAOSP.sh
+++ DerpFestAOSP.sh
@@ -1,5 +1,6 @@
-repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16 --git-lfs
-/opt/crave/resync.sh
+sudo apt install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick protobuf-compiler python3-protobuf lib32readline-dev lib32z1-dev libdw-dev libelf-dev lz4 libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+repo init --no-repo-verify --git-lfs -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16 -g default,-mips,-darwin,-notdefault
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 git clone https://github.com/anshedu/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry
 git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common
 git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry
