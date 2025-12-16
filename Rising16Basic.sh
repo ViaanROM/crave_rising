34340e7050f9814ec4b74a6b7d0b764479540207
ViaanROM <viaan1207@gmail.com>
2025-12-16 12:21:17 +0530

    Update repository initialization and clone commands

--- Rising16Basic.sh
+++ Rising16Basic.sh
@@ -1,13 +1,12 @@
 mkdir Rising16
 cd Rising16
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/RisingOS-Revived/android -b sixteen -g default,-mips,-darwin,-notdefault
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
-git clone https://github.com/ViaanROM/android_device_oneplus_larry -b lineage-22.2 device/oneplus/larry
-git clone https://github.com/ViaanROM/android_device_oneplus_sm6375-common -b lineage-22.2 device/oneplus/sm6375-common
-git clone https://github.com/ViaanROM/proprietary_vendor_oneplus_larry -b lineage-22.2 vendor/oneplus/larry
-git clone https://github.com/ViaanROM/proprietary_vendor_oneplus_sm6375-common -b lineage-22.2 vendor/oneplus/sm6375-common
-git clone https://github.com/ViaanROM/android_kernel_oneplus_sm6375 -b lineage-22.2 kernel/oneplus/sm6375
-git clone https://github.com/ViaanROM/android_hardware_oplus -b lineage-22.2 hardware/oplus
-rm -rf hardware/oplus/hldl/livedisplay
-riseup larry user
-rise b
+repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
+/opt/crave/resync.sh
+git clone https://github.com/anshedu/android_device_oneplus_larry -b lineage-23.0 device/oneplus/larry
+git clone https://github.com/anshedu/android_device_oneplus_sm6375-common -b lineage-23.0 device/oneplus/sm6375-common
+git clone https://github.com/anshedu/proprietary_vendor_oneplus_larry -b lineage-23.0 vendor/oneplus/larry
+git clone https://github.com/anshedu/proprietary_vendor_oneplus_sm6375-common -b lineage-23.0 vendor/oneplus/sm6375-common
+git clone https://github.com/anshedu/android_kernel_oneplus_sm6375 -b lineage-23.0 kernel/oneplus/sm6375
+git clone https://github.com/anshedu/android_hardware_oplus -b lineage-23.0 hardware/oplus
+lunch lineage_larry-user || lunch lineage_larry-bp2a-user
+mka bacon
